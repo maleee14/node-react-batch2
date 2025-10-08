f3fc13e43040212121a6a4512d01bdfad4a703ee
maleee14 <akmalmf14@gmail.com>
2025-10-09 06:32:02 +0800

    tugas hari ke-2

--- tugas/tugas-fundamental-js/function.js
+++ tugas/tugas-fundamental-js/function.js
@@ -3,90 +3,101 @@
  */
 
 function introduce(name, gender, job, age) {
-    if (gender == "laki-laki") {
-        return `Pak ${name} adalah seorang ${job} yang berusia ${age} tahun`
-    } else if (gender == "perempuan"){
-        return `Bu ${name} adalah seorang ${job} yang berusia ${age} tahun`
-    }
+  if (gender == "laki-laki") {
+    return `Pak ${name} adalah seorang ${job} yang berusia ${age} tahun`;
+  } else if (gender == "perempuan") {
+    return `Bu ${name} adalah seorang ${job} yang berusia ${age} tahun`;
+  }
 }
 
-var john = introduce("John", "laki-laki", "penulis", "30")
-console.log(john)
+var john = introduce("John", "laki-laki", "penulis", "30");
+console.log(john);
 
-var sarah = introduce("Sarah", "perempuan", "model", "28")
-console.log(sarah)
+var sarah = introduce("Sarah", "perempuan", "model", "28");
+console.log(sarah);
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 /**
  * 2. Buatlah function yang menerima parameter berupa string (gunakan var text dibawah sebagai parameter) yang mencari karakter atau huruf yang tidak berulang
  */
-var text = "Super Bootcamp Fullstack Dev 2022"
-var txtToLower = text.toLowerCase()
-var txt
-function findUniqueWord(text) {
-    //
+var text = "Super Bootcamp Fullstack Dev 2022";
+function findUniqueWord(str) {
+  str = str.toLowerCase().replaceAll(" ", "");
+  var result = "";
+
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+    if (str.indexOf(char) === str.lastIndexOf(char)) {
+      result += char;
+    }
+  }
+  return result;
 }
 
-console.log('-----------------------------------')
+console.log(findUniqueWord(text));
+
+console.log("-----------------------------------");
 
 /**
  * 3.  buatlah function yang menghasilkan angka terbesar dan angka terkecil dari sebuah array yang berisi data intege
  */
 
 function smallAndBig(angka) {
-    const small = Math.min(...angka)
-    const big = Math.max(...angka)
+  const small = Math.min(...angka);
+  const big = Math.max(...angka);
 
-    console.log(`angka terbesar adalah ${big} dan angka terkecil adalah ${small}`)
+  console.log(
+    `angka terbesar adalah ${big} dan angka terkecil adalah ${small}`
+  );
 }
 
-var angka = [2,3,1,9,12,8,9,7]
-smallAndBig(angka)
+var angka = [2, 3, 1, 9, 12, 8, 9, 7];
+smallAndBig(angka);
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 /**
  * 4. Buatlah function untuk menyusun karakter pada string secara alfabetis
  */
 
 function arrangeString(str) {
-     return str.split('').sort().join('')
+  return str.split("").sort().join("");
 }
 
-console.log(arrangeString("similikiti")) // Output : iiiiiklmst
-console.log(arrangeString("sanbercode")) // Output : abcdeenors
-console.log(arrangeString("")) // Output : ""
+console.log(arrangeString("similikiti")); // Output : iiiiiklmst
+console.log(arrangeString("sanbercode")); // Output : abcdeenors
+console.log(arrangeString("")); // Output : ""
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 /**
  * 5.  mengembalikan nilai true jika kata merupakan palindrome, dan false jika bukan
  */
 
 function palindrome(kata) {
-    var word = kata.split('').reverse().join('')
+  var word = kata.split("").reverse().join("");
 
-    if (kata == word) {
-        return true
-    } else {
-        return false
-    }
+  if (kata == word) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(palindrome('katak')); // true
-console.log(palindrome('blanket')); // false
-console.log(palindrome('nababan')); // true
-console.log(palindrome('haji ijah')); // true
-console.log(palindrome('mister')); // false
+console.log(palindrome("katak")); // true
+console.log(palindrome("blanket")); // false
+console.log(palindrome("nababan")); // true
+console.log(palindrome("haji ijah")); // true
+console.log(palindrome("mister")); // false
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 function angkaPalindrome(num) {
-  num++; 
+  num++;
   while (true) {
     let str = num.toString();
-    let reversed = str.split('').reverse().join('');
+    let reversed = str.split("").reverse().join("");
     if (str === reversed) {
       return num;
     }
@@ -100,11 +111,11 @@ console.log(angkaPalindrome(117)); // 121
 console.log(angkaPalindrome(175)); // 181
 console.log(angkaPalindrome(1000)); // 1001
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 function cekPermutasi(str1, str2) {
-  let clean1 = str1.replace(/\s/g, '').split('').sort().join('');
-  let clean2 = str2.replace(/\s/g, '').split('').sort().join('');
+  let clean1 = str1.replace(/\s/g, "").split("").sort().join("");
+  let clean2 = str2.replace(/\s/g, "").split("").sort().join("");
   return clean1 === clean2;
 }
 
