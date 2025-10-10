c5dd92affcf7a11d8ea35bfcd113219d50e74034
maleee14 <akmalmf14@gmail.com>
2025-10-10 13:12:58 +0800

    tugas hari ke-2

--- tugas/tugas-fundamental-js/looping.js
+++ tugas/tugas-fundamental-js/looping.js
@@ -1,40 +1,40 @@
 /**
- * 1. Pada soal ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while 
+ * 1. Pada soal ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while
  */
 
-console.log("LOOPING PERTAMA")
-var i = 1
+console.log("LOOPING PERTAMA");
+var i = 1;
 while (i <= 20) {
-    if (i % 2 === 0) {
-        console.log(`${i} - I love coding`)
-    }
-    i++
+  if (i % 2 === 0) {
+    console.log(`${i} - I love coding`);
+  }
+  i++;
 }
 
-console.log("LOOPING KEDUA")
-var i = 20 
+console.log("LOOPING KEDUA");
+var i = 20;
 do {
-     if (i % 2 === 0) {
-        console.log(`${i} - I will become a MERN Stack developer`)
-    }
-    i--
-} while (i > 0)
+  if (i % 2 === 0) {
+    console.log(`${i} - I will become a MERN Stack developer`);
+  }
+  i--;
+} while (i > 0);
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 /**
  * 2. Pada soal ini kamu dimint untuk melakukan looping secara selang seling
  */
 
 for (let i = 1; i <= 20; i++) {
-    if (i % 2 === 0) {
-        console.log(`${i} - Angka Genap`)
-    } else {
-        console.log(`${i} - Angka Ganjil`)
-    }
+  if (i % 2 === 0) {
+    console.log(`${i} - Angka Genap`);
+  } else {
+    console.log(`${i} - Angka Ganjil`);
+  }
 }
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 /**
  * 3. Pada soal ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for
@@ -52,26 +52,31 @@ for (let i = 1; i <= 20; i++) {
   }
 }
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 /**
  * Kali ini kamu diminta untuk menampilkan sebuah segitiga dengan tanda pagar (#) dengan dimensi tinggi 7 dan alas 7
  */
 for (let i = 1; i < 8; i++) {
-    let space = ""
-    for (let j = 0; j < i; j++) {
-        space += "#" 
-    }
-    console.log(space)
+  let space = "";
+  for (let j = 0; j < i; j++) {
+    space += "#";
+  }
+  console.log(space);
 }
 
-console.log('-----------------------------------')
+console.log("-----------------------------------");
 
 /**
  * 5. pada soal ini kamu diminta untuk menampilkan huruf konsonan saja dari variabel di bawah ini dengan looping
  */
 
-var sentence = "Fullstack Developer"
-for (i = 0 ; i < sentence.length; i++) {
-    console.log(sentence[i])   
+var sentence = "Fullstack Developer";
+var vowels = "aiueoAIUEO";
+
+for (var i = 0; i < sentence.length; i++) {
+  var huruf = sentence[i];
+  if (!vowels.includes(huruf) && huruf !== " ") {
+    console.log(huruf);
+  }
 }
